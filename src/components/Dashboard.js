d8ca047e474f5721ad351eaffb35ee0527520380
kai li <lk199151@gmail.com>
2020-07-01 10:25:41 -0400

    add answer category

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,62 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import Question from "./Question";
+import {NavLink} from "react-router-dom";
 
 
 class Dashboard extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            isAnswered: false
+        }
+    }
+    handleAnswered = () => {
+        this.setState({isAnswered: true})
+    };
+
+    handleUnAnswered = () => {
+        this.setState({isAnswered: false})
+
+    };
+
     render() {
-        // console.log(this.props)
+        console.log(this.state.isAnswered)
+        const {userUnAnsweredQuestionIds, userAnsweredQuestionIds} = this.props;
         return (
             <div>
 
-                <h3 className='center'>Questions</h3>
+
+                <nav className='nav-answer'>
+                    <ul className='nav-ul'>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active' className='btn' onClick={this.handleUnAnswered}>
+                            Unanswered questions
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active' className='btn' onClick={this.handleAnswered}>
+                            Answered question
+                        </NavLink>
+                    </li>
+                    </ul>
+                </nav>
+
                 <ul className='dashboard-list'>
-                    {this.props.questionIds.map((id) => (
-                        <li key={id}>
-                            <Question id={id}/>
-                        </li>
-                    ))}
+
+                    {this.state.isAnswered ?
+                        userAnsweredQuestionIds.map((id) => (
+                            <li key={id}>
+                                <Question id={id}/>
+                            </li>
+                        )) :
+                        userUnAnsweredQuestionIds.map((id) => (
+                            <li key={id}>
+                                <Question id={id}/>
+                            </li>
+                        ))
+                    }
                 </ul>
 
             </div>
@@ -26,10 +67,41 @@ class Dashboard extends Component {
     }
 }
 
-function mapStateToProps({questionsReducer}) {
+function mapStateToProps({questionsReducer, authedUserReducer, usersReducer}) {
+    // console.log(authedUserReducer)
+    // const answeredQuestions = Object.assign({}, ...Object.entries(questionsReducer).filter(
+    //     ([id, question]) => {
+    //         const isAnswered = question.optionOne.votes.length !== 0 || question.optionTwo.votes.length !== 0
+    //         // console.log(id, question, isAnswered, question.optionOne.votes.length.l)
+    //         return isAnswered
+    //     }
+    // ).map(([id, question]) => ({[id]:question})));
+    //
+    // const unAnsweredQuestions = Object.assign({}, ...Object.entries(questionsReducer).filter(
+    //     ([id, question]) => {
+    //         const isAnswered = question.optionOne.votes.length === 0 && question.optionTwo.votes.length === 0
+    //         return isAnswered
+    //     }
+    // ).map(([id, question]) => ({[id]:question})));
+
+
+    const orderQuestions = (input) => (
+        Object.keys(input).sort((a, b) => input[b].timestamp - input[a].timestamp)
+    );
+
+
+    const userAnsweredQuestionIdsRaw = Object.keys(usersReducer[authedUserReducer].answers);
+    const questionIds =  orderQuestions(questionsReducer);
+
+    const userUnAnsweredQuestionIds = questionIds.filter((id) => !(userAnsweredQuestionIdsRaw.includes(id)))
+    const userAnsweredQuestionIds = questionIds.filter((id) => (userAnsweredQuestionIdsRaw.includes(id)))
+    console.log('userAnsweredQuestionIds: ', userAnsweredQuestionIds, userUnAnsweredQuestionIds, questionIds)
+
+
     return {
-        questionIds: Object.keys(questionsReducer).sort((a, b) => questionsReducer[b].timestamp - questionsReducer[a].timestamp)
-        // questionIds: questionsReducer
+        questionIds,
+        userAnsweredQuestionIds,
+        userUnAnsweredQuestionIds
     }
 }
 
